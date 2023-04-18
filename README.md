b1540cd01599865eadc32cd2fca4b13c1b03fdaa
zeio <zeio99guo@gmail.com>
2023-04-18 23:09:03 +0100

    :fire:New_version_0.5

--- README.md
+++ README.md
@@ -4,9 +4,7 @@ My site is live at https://zihao-guo.github.io
 
 # !!! ing  !!! 
 - [ ] Project section
-    - [ ] Modify to pop-up
     - [ ] Select different modules other modules disappear
     - [ ] Add projet (modify the introduction of the RESEARCH section)
-- [ ] Modify personal information and upload to github homepage
-    - [ ] Change the link
 - [ ] To be revised: house price forecasting + RL + mathematical modeling
+- [ ] Check the link
--- js/script.js
+++ js/script.js
@@ -173,3 +173,30 @@
 
 
 })(jQuery);
+
+
+
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = "block";
+}
+
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
+
+function closeModalByDot(closeDot) {
+    var modal = closeDot.closest('.modal');
+    modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    const modals = document.getElementsByClassName("modal");
+    for (let i = 0; i < modals.length; i++) {
+        if (event.target == modals[i]) {
+            modals[i].style.display = "none";
+        }
+    }
+}
